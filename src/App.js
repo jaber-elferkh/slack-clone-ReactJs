cb8afdd020650714bfa15fc2e8b66f501bee59d2
jaeel29 <elferkhjaber@gmail.com>
2021-02-21 16:28:28 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Slaaack</h1>
+    <div className="container">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/">
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <MenuIcon className="menu" />
+        <AccessTimeIcon className="time" />
+      </HeaderLeft>
+
+      <HeaderSearch>
+        <input type="text" placeholder="Search Slack" />
+      </HeaderSearch>
+
+      <HeaderRight>
+        <HeaderAvatar />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  /* outline: 2px solid red; */
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
+  color: #ababad;
+
+  > .time {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  /* outline: 2px solid red; */
+  flex: 0.4;
+  border-radius: 5px;
+  background-color: #1f1e23;
+  border: 1px solid #ababad;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    min-width: 30vw;
+  }
+
+  > input[placeholder] {
+    color: #ababad;
+  }
+`;
+
+const HeaderRight = styled.div`
+  /* outline: 2px solid red; */
+  flex: 0.3;
+  margin-right: 20px;
+  align-items: flex-end;
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  margin-left: auto;
+  width: 40px;
+  height: 40px;
+
+  :hover {
+    opacity: 0.5;
+  }
+`;
