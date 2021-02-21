48956c4bed310d9433108fa370d57bd04b3c3fc0
jaeel29 <elferkhjaber@gmail.com>
2021-02-21 14:26:10 +0100

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCX5RuAQQY0TdwFq0_ZpeOiLIy5a24UT6I',
+  authDomain: 'slack-clone-ba18a.firebaseapp.com',
+  projectId: 'slack-clone-ba18a',
+  storageBucket: 'slack-clone-ba18a.appspot.com',
+  messagingSenderId: '479505139193',
+  appId: '1:479505139193:web:e7c94f6bb2d1f60492a928',
+};
